92d13f672c4f449ca3e75f1e041761e2ebe4204e
Imaad Uddin <imaaduddin27@gmail.com>
2022-03-29 00:30:23 -0500

    creating player/computer moves functions

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)] # we will use a single list to rep 3x3 board
@@ -8,4 +11,56 @@ class TicTacToe:
             print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
     
+    def available_moves(self):
+        # returns []
+        moves = []
+        for (i, spot) in enumerate(self.board):
+            # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
+            if spot == ' ':
+                moves.append(i)
+            return moves 
+    
+    def empty_squares(self):
+        return ' ' in self.board
+    
+    def num_empty_squares(self):
+        return self.board.count(' ')
+    
+    def make_move(self, square, letter):
+        # if valid move, then make the move (assign square to letter)
+        # then return True, if invalid, return false
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True 
+        return False 
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+    
+    letter = 'X' # starting letter 
+    # iterate while the game still has empty squares
+    # (we don't have to worry about a winner b/c we'll just return that which breaks the loop)
+    while game.empty_squares():
+        if letter == '0':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        
+        # let's define a function to make a move! 
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f'makes a move to square {square}')
+                game.print_board()
+                print('') # juts empty line
+            
+            # after we make our move, we need to alternate letters
+            letter = 'O' if letter == 'X' else 'X' # switches player 
--- main.py
+++ main.py
@@ -15,12 +15,28 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
     
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we're going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, then we say its invalid
+            # if the spot is not available on the board, we also say its involved 
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True # if these are successful, then good!
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
         
     
